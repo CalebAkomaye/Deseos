e4dcda20caa1695e5f0746cf6bdb86e997d2930d
Caleb Akomaye <calebakomaye@outlook.com>
2024-05-08 15:19:09 +0100

    Added Authentication Functionality

--- deseos/app/(main)/_components/hero.tsx
+++ deseos/app/(main)/_components/hero.tsx
@@ -1,9 +1,19 @@
+'use client';
+
 import React from 'react';
 import { Undo } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { FaGoogle } from 'react-icons/fa';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { auth } from '@/utils/firebase';
 
 const Hero = () => {
+  const googleProvider = new GoogleAuthProvider();
+  const googleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+    } catch (error) {}
+  };
   return (
     <div className=' md:px-6 px-3'>
       <div className='inline-flex flex-col gap-y-4 items-start'>
@@ -14,9 +24,9 @@ const Hero = () => {
             <br />
             for copy writers
           </h1>
-          <p>Never miss a read, share, tale, and save your favorite books</p>
+          <p>Never miss a word, share a story, and save your favorite copies</p>
         </div>
-        <Button className=' mt-7'>
+        <Button className=' mt-7' onClick={googleLogin}>
           <FaGoogle />{' '}
           <span className='inline-block ml-2'>Sign in with Google</span>
         </Button>
